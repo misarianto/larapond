518179ed4148c31e5a089364cda531e5f11e81a6
misarianto <amikrian8@gmail.com>
2023-05-05 11:02:34 +0700

    membuat perintah hapus folder directory file temporary dan row database

--- app/Http/Controllers/DeleteTemporaryImageController.php
+++ app/Http/Controllers/DeleteTemporaryImageController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Storage;
+use App\Models\TemporaryImage;
 
 class DeleteTemporaryImageController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        $temporaryImage = TemporaryImage::where('folder', request()->getContent())->first();
+        if($temporaryImage){
+            Storage::deleteDirectory('images/tmp/'.$temporaryImage->folder);
+            $temporaryImage->delete();
+        }
+
+        return response()->noContent();
     }
 }
