9008276344bd337b6365bc7565e8a40d237031ba
misarianto <amikrian8@gmail.com>
2023-05-05 11:40:14 +0700

    membuat store post and image,delete temporaryImage success store

--- app/Http/Controllers/StorePostController.php
+++ app/Http/Controllers/StorePostController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Image;
+use App\Models\TemporaryImage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class StorePostController extends Controller
@@ -11,6 +16,41 @@ class StorePostController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        try{
+
+            $temporaryImages = TemporaryImage::all(); 
+
+            if($validator->fails()){
+                foreach($temporaryImages as $temporaryImage){
+                    Storage::deleteDirectory('images/tmp/'.$temporaryImage->folder);
+                    $temporaryImage->delete();
+                }
+                return redirect('/')->withErrors($validator)->withInput();
+            }
+
+            $post = Post::create($validator->validated());
+
+            foreach($temporaryImages as $temporaryImage){
+                Storage::copy('images/tmp/'.$temporaryImage->folder .'/'. $temporaryImage->file, 'images/' . $temporaryImage->folder .'/'. $temporaryImage->file);
+                Image::create([
+                    'post_id' => $post->id,
+                    'name' => $temporaryImage->file,
+                    'path' => $temporaryImage->folder .'/'. $temporaryImage->file
+                ]);
+                Storage::deleteDirectory('images/tmp/'.$temporaryImage->folder);
+                $temporaryImage->delete();
+            }
+
+            return redirect('/');
+
+        }catch(\Exception $e){
+            dd($e->getMessage());
+        }
+
     }
 }
